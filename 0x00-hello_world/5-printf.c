187b6b1522f1d40b2d0b3944f23eb7e908cd6d25
FurankieSama <franknwokoma1@gmail.com>
2024-07-23 14:04:35 +0100

    Print using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print using printf function
+ * Return: Zero
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is  piece of art,\n");
+return (0);
+}
