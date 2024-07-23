24ba28a604c4b53a401e35e4143c120ecfcdf3ea
FurankieSama <franknwokoma1@gmail.com>
2024-07-23 13:35:15 +0100

    Print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print text using puts function
+ * Return: Zero
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
