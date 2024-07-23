c83c9830235734b974d5d255bd25fc02e0af35b9
FurankieSama <franknwokoma1@gmail.com>
2024-07-23 15:11:16 +0100

    Printing to the terminal without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print to terminal with write function
+ * Return: Zero
+ */
+
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
