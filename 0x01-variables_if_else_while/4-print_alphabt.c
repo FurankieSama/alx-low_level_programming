fcba7a0d361610fcd76b5c944ef8c13b652fa79a
FurankieSama <franknwokoma1@gmail.com>
2024-07-25 10:41:47 +0100

    PRINT ALPHABETS EXCLUDING E AND Q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabets except p and q
+ * Return: Zero
+ */
+int main(void)
+{
+char alp;
+
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+
+if (alp != 'e' && alp != 'q')
+{
+putchar(alp);
+}
+}
+putchar('\n');
+return (0);
+
+}
