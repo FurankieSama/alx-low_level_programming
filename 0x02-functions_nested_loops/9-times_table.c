a56592fccb18434ed50937d44cfad23429aaaa04
FurankieSama <franknwokoma1@gmail.com>
2024-07-26 16:38:06 +0100

    FUNCTIONS IN C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,28 +9,26 @@ void times_table(void)
 int num, var, res;
 for (num = 0; num < 10; num++)
 {
-for (var= 0; var < 10; var++)
+for (var = 0; var < 10; var++)
 {
 res = num * var;
 if (var != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (res >= 10)
-			{
-				_putchar((res / 10) + '0');
-				_putchar((res % 10) + '0');
-			}
-			else if (res < 10 && var != 0)
-			{
-				_putchar(' ');
-				_putchar((res % 10) + '0');
-			}
-			else
-				_putchar((res % 10) + '0');
-		}
-
-
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && var != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
+}
 }
 }
