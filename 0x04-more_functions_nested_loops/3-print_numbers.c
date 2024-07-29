65b6a949d4bbe08fc16472f1d0a324c618fe6f37
FurankieSama <franknwokoma1@gmail.com>
2024-07-29 19:16:12 +0100

    Print all digits from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print digits from 0 to 9
+ * Return: Zero
+ */
+void print_numbers(void)
+{
+char c;
+while (c < 9)
+{
+_putchar(c);
+}
+_putchar('\n');
+
+}
