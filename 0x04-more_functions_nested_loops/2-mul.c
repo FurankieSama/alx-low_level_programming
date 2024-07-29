c2f1aad4cd8944a149829682b969bb543020e903
FurankieSama <franknwokoma1@gmail.com>
2024-07-29 19:07:29 +0100

    Function multiplying two variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function multiplying 2 variables
+ * @a: variable to be multiplied
+ * @b: variable to be multiplied
+ * Return: c
+ */
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
