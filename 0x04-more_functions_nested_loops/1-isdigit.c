cabd425cd84db5da84053137f6af2f432dfa7516
FurankieSama <franknwokoma1@gmail.com>
2024-07-29 18:56:55 +0100

    Function checking if variable is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - check if variable is digit
+ * @c: variable to check
+ * Return: 1 of digit, else return 2
+ */
+int _isdigit(int c)
+{
+return (c >= 0 || c <= 10);
+}
