4b49eaf4ba35ae667ae93c673a92378ddf18eb48
FurankieSama <franknwokoma1@gmail.com>
2024-07-30 09:37:10 +0100

    Checks if var is positive, negative or neutral

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - checks if variable is positive or negative
+ * Return: Zero
+ */
+int main(void)
+{
+int num;
+num = 0;
+positive_or_negative(num);
+return (0);
+}
