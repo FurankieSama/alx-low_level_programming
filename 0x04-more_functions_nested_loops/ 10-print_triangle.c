26844f012731fa7d0e2175e252a79085e643cb86
FurankieSama <franknwokoma1@gmail.com>
2024-08-02 13:08:55 +0100

    Function printing right-sided triangle

---
+++ 0x04-more_functions_nested_loops/ 10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints a right-sided triangle
+ * @size: variable determining the size of the triangle
+ * Return: Zero
+ */
+
+void print_triangle(int size)
+{
+    int first;
+    int secnd;
+    int thrd;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (first = 0; first < size; first++)
+{
+for (secnd = first; secnd < size; secnd++)
+{
+_putchar(' ');
+}
+for (thrd = 0; thrd <= first; thrd++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
