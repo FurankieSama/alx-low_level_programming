513c57c46688baf19e9ea4664df4388c316a0e69
FurankieSama <franknwokoma1@gmail.com>
2024-08-02 13:22:54 +0100

    FizzBuzz in C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 int num;
 for (num = 1; num <= 100; num++)
 {
-if (num % 15 == 0)
+if (num % 3 == 0 && num % 5 == 0)
 {
 printf("FizzBuzz ");
 }
@@ -21,6 +21,11 @@ else if (num % 5 == 0)
 {
 printf("Buzz ");
 }
+else if (num == 1)
+{
+printf("%d ", num);
+}
+
 else
 {
 printf("%d ", num);
